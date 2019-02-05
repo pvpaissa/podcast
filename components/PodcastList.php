e070fd35605ace2f52ce68b053c1a22e3ce0ba0d
Paul Lovato <plovato@gmail.com>
2019-02-05 16:48:36 -0700

    Cannot name class 'List'.

---
+++ components/PodcastList.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cleanse\Podcast\Components;
+
+use Cms\Classes\ComponentBase;
+use Cleanse\Podcast\Models\Podcast;
+
+class PodcastList extends ComponentBase
+{
+    /**
+     * @var \Cleanse\Podcast\Models\Podcast The post model used for display.
+     */
+    public $episodes;
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'PvPaissa Podcast List',
+            'description' => 'Grabs the podcasts in your database.'
+        ];
+    }
+
+    public function onRun()
+    {
+        $this->episodes = $this->page['episodes'] = $this->loadPodcasts();
+    }
+
+    protected function loadPodcasts()
+    {
+        return Podcast::all();
+    }
+}
